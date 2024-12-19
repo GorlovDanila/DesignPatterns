faefae86a75562860e9ebbb0278fe68e0eeed19d
d.gorlov <d.gorlov@tinkoff.ru>
2024-12-19 15:21:44 +0300

    Initial commit

---
+++ app/src/main/java/com/example/designpatterns/creational/singleton/Singleton.kt
@@ -0,0 +1,25 @@
+package com.example.designpatterns.creational.singleton
+
+class Singleton private constructor(private val value: String) {
+
+    fun getValue() = value
+
+    companion object {
+
+        private var instance: Singleton? = null
+
+        fun getInstance(value: String): Singleton {
+            if (instance == null) {
+                instance = Singleton(value)
+            }
+            return requireNotNull(instance) { "instance is null" }
+        }
+    }
+}
+
+fun main() {
+    val a = Singleton.getInstance("a")
+    val b = Singleton.getInstance("b")
+    println(a.getValue())
+    println(b.getValue())
+}
